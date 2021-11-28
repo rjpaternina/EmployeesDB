9126fdb4b17543bb134f1833268106f3a86154fe
rjpaternina <rjpaternina@gmail.com>
2021-11-28 12:21:36 -0500

    Second commit

--- src/controllers/Empleado.controllers.js
+++ src/controllers/Empleado.controllers.js
@@ -38,4 +38,46 @@ EmpleadoCtrl.ListarId = async (req, res)=>{
 
 }
 
+
+EmpleadoCtrl.empleadoDeunJefe = async (req, res)=>{
+
+    const id = req.params.id;
+    const respuesta = await Empleado.find({jefe: id});
+    res.json (respuesta)
+}
+
+
+EmpleadoCtrl.eliminarEmpleado = async (req, res)=>{
+
+    const id =req.params.id;
+    await Empleado.findByIdAndRemove({_id: id})
+    res.json({
+        
+        mensaje: 'Empleado eliminado'
+    })
+}
+
+
+EmpleadoCtrl.actualizarEmpleado = async (req, res)=>{
+
+    const id = req.params.id   
+    await Empleado.findByIdAndUpdate({_id: id}, req.body)
+
+
+    res.json({
+
+        mensaje: 'Empleado actualizado'
+    })
+}
+
+
+EmpleadoCtrl.buscarEmpleado  = async (req, res)=>{
+
+    const {nombres, id} = req.params
+
+    const respuesta = await Empleado.find({nombres:{$regex: ".*"+nombres+".*"}, jefe:id})
+    
+    
+}
+
 module.exports = EmpleadoCtrl
--- src/routes/Empleado.routes.js
+++ src/routes/Empleado.routes.js
@@ -1,4 +1,5 @@
 const {Router} = require ('express')
+const { eliminarEmpleado } = require('../controllers/Empleado.controllers')
 const router = Router ()
 
 const EmpleadoCtrl = require ('../controllers/Empleado.controllers')
@@ -6,5 +7,9 @@ const EmpleadoCtrl = require ('../controllers/Empleado.controllers')
 router.post('/crear', EmpleadoCtrl.crear)
 router.get('/listarEmpleados', EmpleadoCtrl.Listar)
 router.get('/listar/:id', EmpleadoCtrl.ListarId)
+router.get('/listarEmpleadosJefe/:id', EmpleadoCtrl.empleadoDeunJefe)
+router.delete('/eliminarEmpleado/:id', EmpleadoCtrl.eliminarEmpleado)
+router.put('/actualizarEmpleado/:id', EmpleadoCtrl.actualizarEmpleado)
+router.get('/buscarEmpleado/:nombres/:id', EmpleadoCtrl.buscarEmpleado)
 
 module.exports = router
